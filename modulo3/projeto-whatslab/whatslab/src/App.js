d396f8b747bc51751f5a87885bd5852e515601cd
raulrita08 <raul.vinicius@live.com>
2022-02-04 20:40:34 -0300

    Iniciando projeto WhatsLab

---
+++ modulo3/projeto-whatslab/whatslab/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ParteCima from './components/ParteCima';
+
+function App() {
+  return (
+    <ParteCima>
+
+    </ParteCima>
+  )
+}
+
+export default App;
---
+++ modulo3/projeto-whatslab/whatslab/src/components/ParteCima.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const CaixaHeader = styled.div `
+background-color: blue;
+height: 50px;
+`
+
+
+function ParteCima (props) {
+    return (
+        <CaixaHeader>
+
+        </CaixaHeader>
+    )
+}
+
+export default ParteCima;
