57363eaffe53a3a17b0bcbab41394e4bcb282a3a
raulrita08 <raul.vinicius@live.com>
2022-03-27 13:52:35 -0300

    Renderização condicional no app funcionando

--- modulo3/projeto-astromatch/astromatch/src/App.js
+++ modulo3/projeto-astromatch/astromatch/src/App.js
@@ -1,13 +1,34 @@
-import React from "react";
+import React, {useState} from "react";
 import ListaMatches from "./pages/ListaMatches";
 import TelaInicial from "./pages/TelaInicial";
 
 const App = () => {
+
+  const [tela, setTela] = useState("inicial")
+
+  const trocaTela = () => {
+
+    switch (tela) {
+      case "inicial":
+        return <TelaInicial irParaListaMatches={irParaListaMatches} />
+      case "lista":
+        return <ListaMatches irParaTelaInicial={irParaTelaInicial} />
+      default:
+        return <p>Algo deu errado!</p>
+    }
+  }
+
+  const irParaTelaInicial = () => {
+    setTela("inicial")
+  }
+
+  const irParaListaMatches = () => {
+    setTela("lista")
+  }
+
   return (
     <div>
-      Vamos começar!
-      <ListaMatches />
-      <TelaInicial />
+      {trocaTela()}
     </div>
   );
 }
