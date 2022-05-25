e56d1253c5f35078bbb38fa6124a3bed462893bc
raulrita08 <raul.vinicius@live.com>
2022-05-24 21:58:05 -0300

    exercicios refatoracao de js para ts

---
+++ modulo5/intro-typescript/src/exercicio2.ts
@@ -0,0 +1,11 @@
+function imprimeTresCoresFavoritas() {
+    // const cor1:string = prompt("Insira sua primeira cor favorita")
+    const cor1:string = "verde"
+    // const cor2:string = prompt("Insira sua segunda cor favorita")
+    const cor2:string = "prata"
+    // const cor3:string = prompt("Insira sua terceira cor favorita")
+    const cor3:string = "dourado"
+    console.log(cor1, cor2, cor3)
+    
+ }
+ 
