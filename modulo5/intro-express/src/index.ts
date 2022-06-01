04193972b67678c2afa245d0765187666cf6b804
raulrita08 <raul.vinicius@live.com>
2022-06-01 16:24:40 -0300

    exercicio finalizado

--- modulo5/intro-express/src/index.ts
+++ modulo5/intro-express/src/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import { AddressInfo } from "net";
 import { Request, Response } from "express";
+import { posts, users } from "./data";
 
 const app = express();
 app.use(express.json());
@@ -14,8 +15,53 @@ const server = app.listen(process.env.PORT || 3003, () => {
   }
 });
 
-//Exercicio 1
-
 app.get('/', (req: Request, res: Response) => {
     res.status(200).send("Hello from express")
 })
+
+app.get('/users', (req : Request, res : Response) => {
+    try {
+        const person = users
+        const listaUsuarios = person.map((cadaUsuario) => cadaUsuario)
+
+        res.status(200).send(listaUsuarios)
+    }
+
+    catch {
+        res.status(400).end("Usuários não encontrados")
+    }
+})
+
+app.get('/posts', (req : Request, res : Response) => {
+    try {
+        const arrayPosts = posts
+        const listaPosts = arrayPosts.map((cadaPost) => cadaPost)
+
+        res.status(200).send(listaPosts)
+    }
+
+    catch {
+        res.status(400).end("Posts não encontrados")
+    }
+})
+
+app.get('/posts/:userId', (req : Request, res : Response) => {
+    
+        const userId = Number (req.params.userId)
+        const postsUser = posts.find((element) => {
+            element.userId === userId
+        })
+
+        if (!postsUser) {
+            res.status(404).send("Posts do usuário não foi encontrado")
+        } else {
+            res.status(200).send(postsUser)
+        }
+
+        res.status(200).send(postsUser)
+    
+
+    
+})
+
+
