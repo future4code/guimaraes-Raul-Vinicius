9ff4aa363bb0ac1c18a7123f79fa83b6a58e118a
raulrita08 <raul.vinicius@live.com>
2022-06-08 19:46:58 -0300

    exercicios 1 a 3 ok

---
+++ modulo5/fluxo-de-dados-backend/src/data.ts
@@ -0,0 +1,25 @@
+// import { v4 as generateId } from 'uuid';
+
+type Products = {
+    id: string,
+    name: string,
+    price: number,
+}
+
+export const products: Products[] = [
+    {
+        id: "1",
+        name: "Air Jordan Low 1",
+        price: 2500.00
+    },
+    {
+        id: "2",
+        name: "Vans Old School",
+        price: 450.00
+    },
+    {
+        id: "3",
+        name: "Adidas Super Star",
+        price: 600.00
+    }
+] 
--- modulo5/fluxo-de-dados-backend/src/index.ts
+++ modulo5/fluxo-de-dados-backend/src/index.ts
@@ -1,10 +1,45 @@
 import express from "express";
-
 import { AddressInfo } from "net";
+import { Request, Response } from "express";
+import { v4 as generateId } from 'uuid';
+import { products } from "./data";
 
 const app = express();
-
 app.use(express.json());
+
+app.get("/test", (req: Request, res: Response)=> {
+    res.status(200).send("Testando funcionamento da API")
+})
+
+//Create products 
+
+app.post("/products/create", (req:Request, res:Response) => {
+    try {
+        // const id = Math.random().toString()
+        const id = generateId().toString()
+        const name = req.body.name
+        const price = req.body.price
+
+        products.push(
+            {
+                id : id,
+                name : name,
+                price : price
+            }
+        ) 
+        res.status(201).send(products)
+    }
+
+    catch {
+        res.status(400).end("Alguma informação está errada")
+    }
+})
+
+
+
+
+
+
 
 const server = app.listen(process.env.PORT || 3003, () => {
   if (server) {
