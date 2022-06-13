20f29a5cb2ce463e9b75f4aef99f3885c837a672
raulrita08 <raul.vinicius@live.com>
2022-06-13 18:15:43 -0300

    funcionalidades basicas ok

--- modulo5/sistema-bancario/src/data/account.ts
+++ modulo5/sistema-bancario/src/data/account.ts
@@ -1,25 +1,61 @@
+enum DESCRIPTION {
+    DEBIT = "Debit",
+    CREDIT = "Credit"
+}
+
 export type Transactions = {
     date : string,
-    description : string,
+    description: DESCRIPTION,
     value : number
 }
 
 export type Account = {
-    nome : string,
+    name : string,
     cpf : string,
-    dataNascimento : string,
-    saldo : number,
-    // transactions : Transactions
+    dateOfBirthday : string,
+    balance : number,
+    extract : Transactions[]
 }
 
 
 
 export const users : Account[] = [
     {
-        nome : "Raul",
+        name : "Raul",
         cpf : "123.123.123-12",
-        dataNascimento : "09/09/1993",
-        saldo : 1000,
+        dateOfBirthday : "09/09/1993",
+        balance : 1000,
+        extract : [
+            {
+                date : "01/01/2022",
+                description : DESCRIPTION.CREDIT,
+                value : 130
+            }, 
+            {
+                date : "02/01/2022",
+                description : DESCRIPTION.DEBIT,
+                value : 50  
+            }
+        ]
+    },
+
+    {
+        name : "Sara",
+        cpf : "324.456.789-12",
+        dateOfBirthday : "01/07/1998",
+        balance : 500,
+        extract : [
+            {
+                date : "01/01/2022",
+                description : DESCRIPTION.CREDIT,
+                value : 30
+            }, 
+            {
+                date : "02/01/2022",
+                description : DESCRIPTION.DEBIT,
+                value : 70  
+            }
+        ]
     }
 ]
 
