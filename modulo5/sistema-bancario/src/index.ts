aae6b0f06b9e1e9401e46ffd91af60680841b39b
raulrita08 <raul.vinicius@live.com>
2022-06-15 17:03:48 -0300

    Desafios 1 e 2

--- modulo5/sistema-bancario/src/index.ts
+++ modulo5/sistema-bancario/src/index.ts
@@ -32,24 +32,52 @@ app.get('/users', (req: Request, res:Response) => {
  
 })
 
+//GET BALANCE BY CPF
+app.get('/users/balance/:cpf', (req: Request, res:Response) => {
+
+  let errorCode = 500
+  const cpf = req.params.cpf
+
+  try {
+
+    if (!cpf) {
+      errorCode = 422
+      throw new Error("O CPF deve ser informado.")
+    }
+
+    for (let user of users) {
+      if (user.cpf === cpf) {
+        res.status(200).send(`O saldo do cliente ${user.name} é: ${user.balance}`)
+      } else {
+        throw new Error("O CPF informado é inválido. Tente novamente!")
+      }
+    }
+    
+  } catch (error : any) {
+    res.status(errorCode).send(error.message)
+  }
+ 
+})
 
 //CREATE NEW USER
 app.post('/users/register', (req: Request, res:Response) => {
   
-  const name = req.body.name 
-  const cpf = req.body.cpf 
+  const name = req.body.name as string
+  const cpf = req.body.cpf as string
   const dateOfBirthday = req.body.dateOfBirthday
   const yearBirthSeparate:string = dateOfBirthday.split('/')
-  const dayBirth = Number(yearBirthSeparate[0])
-  const monthBirth = Number(yearBirthSeparate[1])
+  // const dayBirth = Number(yearBirthSeparate[0])
+  // const monthBirth = Number(yearBirthSeparate[1])
   const yearBirth = Number(yearBirthSeparate[2])
   let age:number = year - yearBirth
   let errorCode = 500
 
   try { 
 
-    if(age < 18) {
-      throw new Error("É necessário ser maior de idade para abrir uma conta!")
+    for (let user of users) {
+      if (user.cpf === cpf) {
+        throw new Error("O CPF informado já existe em nosso banco de dados.")
+      }
     }
 
     if(age >= 18) {
@@ -63,9 +91,11 @@ app.post('/users/register', (req: Request, res:Response) => {
       }  
       users.push(newUser)
       res.status(200).send(newUser)
-    } 
+    } else {
+      throw new Error("É necessário ser maior de idade para abrir uma conta.")
+    }
       
-  } catch (error:any) {
+  } catch (error : any) {
     res.status(errorCode).send(error.message)
   }
 })
