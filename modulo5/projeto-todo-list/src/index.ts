2c1c7b1b0ce5553786b7e8080db87e319771bea9
raulrita08 <raul.vinicius@live.com>
2022-06-26 15:59:05 -0300

    endpoint 1 ok. CreateUser

--- modulo5/projeto-todo-list/src/index.ts
+++ modulo5/projeto-todo-list/src/index.ts
@@ -1,8 +1,41 @@
 import connection from "./connection";
 import app from "./app";
 import { Request, Response } from "express";
+import { v4 as generateId } from "uuid"
 
 // TESTANDO FUNCIONAMENTO DA API
 app.get('/test', (req:Request, res:Response) => {
     res.status(200).send("Api funcionando!")
 });
+
+// ENDPOINTS EM CONEXÃO COM O BANCO 
+
+//1. Criar usuário
+
+app.post('/user', async(req:Request, res:Response) => {
+    let errorCode = 400
+    try {
+        const id = generateId()
+        const {name, nickname, email} = req.body 
+
+        if(!name || !nickname || !email) {
+            errorCode = 401
+            throw new Error("Todos os campos são obrigatórios.")
+        }
+
+        await connection.raw(`
+            INSERT INTO TodoListUser (id, name, nickname, email)
+            VALUES(
+                "${id}",
+                "${name}",
+                "${nickname}",
+                "${email}"
+            )
+        `)
+        res.status(200).send(`Usuário criado com sucesso!`);
+
+    } catch (error:any) {
+        console.log(error)
+        res.status(errorCode).send(error.message);
+    }
+})
